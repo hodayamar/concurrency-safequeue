3c2694cac9cf657d29b568d6eaf1db1ed199a0f6
Hodaya Marciano <hodayamar2000@gmail.com>
2018-12-21 05:57:59 -0800

    safe thread queue

--- SafeQueue.h
+++ SafeQueue.h
@@ -1,8 +1,106 @@
-//
-// Created by hodaya on 12/21/18.
-//
-
 #ifndef EXCELLENTEAM_ELLA_CONCURRENCY_SAFEQUEUE_HODAYAMAR_SAFEQUEUE_H
 #define EXCELLENTEAM_ELLA_CONCURRENCY_SAFEQUEUE_HODAYAMAR_SAFEQUEUE_H
+
+#include <iostream>         //std
+#include <semaphore.h>
+#include <queue> // std::queue
+#include <unistd.h> // sleep
+
+template< class T, size_t SIZE = 3 >
+class SafeQueue
+{
+public:
+
+    SafeQueue();
+    ~SafeQueue();
+    T pop();
+    void push( const T& );
+
+private:
+
+    std::queue< T > m_safeQueue;
+    sem_t semPop;
+    sem_t semPush;
+    pthread_mutex_t mtx;
+
+    void initSync();
+    void deleteSync();
+};
+
+
+template< class T, size_t SIZE >
+SafeQueue< T, SIZE >::SafeQueue()
+{
+    initSync();
+}
+
+template< class T, size_t SIZE >
+SafeQueue< T, SIZE >::~SafeQueue()
+{
+    deleteSync();
+}
+
+template< class T, size_t SIZE >
+void SafeQueue< T, SIZE >::initSync()
+{
+    sem_init( &semPush, 0, SIZE );
+    sem_init( &semPop, 0, 0 );
+    if ( pthread_mutex_init ( &mtx, NULL ) != 0 )
+    {
+        std::cout << "\n mutex init failed\n" << std::endl;
+        deleteSync();
+    }
+}
+
+template< class T, size_t SIZE >
+void SafeQueue< T, SIZE >::deleteSync()
+
+{
+    sem_destroy( &semPush );
+    sem_destroy( &semPop );
+    pthread_mutex_destroy( &mtx );
+}
+
+template< class T, size_t SIZE >
+T SafeQueue< T, SIZE >::pop()
+{
+    T tmp;
+
+    sem_wait( &semPop );
+
+    pthread_mutex_lock( &mtx );
+
+    tmp = m_safeQueue.front();
+    m_safeQueue.pop();
+    std::cout << "Extract ---  " << tmp << std::endl;
+    sleep(1);
+
+
+    pthread_mutex_unlock( &mtx );
+
+    sem_post( &semPush );
+
+    return tmp;
+}
+
+
+template< class T, size_t SIZE >
+void SafeQueue< T, SIZE >::push( const T& elementToPush )
+{
+
+    sem_wait( &semPush );
+
+    pthread_mutex_lock( &mtx );
+
+    m_safeQueue.push( elementToPush );
+    std::cout << "Push ---- " << elementToPush << std::endl;
+    sleep(1);
+
+
+    pthread_mutex_unlock( &mtx );
+
+    sem_post( &semPop );
+
+}
 
 #endif //EXCELLENTEAM_ELLA_CONCURRENCY_SAFEQUEUE_HODAYAMAR_SAFEQUEUE_H
--- main.cpp
+++ main.cpp
@@ -1,4 +1,73 @@
-//
-// Created by hodaya on 12/21/18.
-//
+#include <stdlib.h>
+#include <pthread.h>
 
+#include "SafeQueue.h"
+
+#define NUM_OF_THREAD 4
+
+
+void* Poping( void* queue )
+{
+    while( true )
+
+        ( ( SafeQueue< int, 5 >* ) queue )-> pop();
+}
+
+
+void* Pushing( void* queue )
+{
+    int rand_a;
+    while( true )
+    {
+        rand_a = rand();
+        ( ( SafeQueue< int, 5 >* ) queue )-> push( rand_a );
+    }
+}
+
+void* randomPushPop( void* queue )
+{
+    int randPopPush, numToInsert, popNum;
+
+    while( true )
+    {
+        randPopPush = rand();
+
+        if( randPopPush < 1000 )
+        {
+            numToInsert = rand();
+            ( ( SafeQueue< int, 5 >* ) queue )-> push( randPopPush );
+            std::cout << "Push ---- " << numToInsert << std::endl;
+        }
+
+        {
+            popNum = ( ( SafeQueue< int, 5 >* ) queue )-> pop();
+            std::cout << "Push ---- " << popNum << std::endl;
+        }
+
+    }
+}
+
+int main()
+{
+    SafeQueue< int, 10 > mySafeQueue;
+
+    pthread_t threads [ NUM_OF_THREAD ], threadForPush, threadForPop;
+
+    pthread_create( &threadForPush, NULL, Pushing, ( void* )&mySafeQueue );
+    pthread_create( &threadForPop, NULL, Poping, ( void* )&mySafeQueue );
+
+    for ( int i = 0 ; i < NUM_OF_THREAD ; i++ )
+
+        pthread_create( &threads[i], NULL, randomPushPop, ( void* )&mySafeQueue );
+
+    for ( int i = 0 ; i < NUM_OF_THREAD ; i++ )
+
+        pthread_join( threads[i], NULL );
+
+
+
+    pthread_join( threadForPop,NULL );
+    pthread_join( threadForPush, NULL );
+
+    return 0;
+}
